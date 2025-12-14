ffabe71decdbdd98bdd83a1abe733ef82f38def1
argel6767 <argel6767@gmail.com>
2025-12-14 01:28:55 -0500

    update run_all_services

--- bin/run_all_services.sh
+++ bin/run_all_services.sh
@@ -6,6 +6,12 @@ touch scheduler-engine/.env
 docker compose build --no-cache
 docker compose up -d
 
-docker compose wait
+echo "Waiting for database to be healthy..."
+while [ "$(docker inspect -f '{{.State.Health.Status}}' \
+  $(docker compose ps -q db))" != "healthy" ]; do
+  sleep 2
+done
+
+echo "All services are up"
 
 docker compose down
