a2f197bcd3f91dfe76adbc4888dc8471a0377947
argel6767 <argel6767@gmail.com>
2025-12-16 16:22:57 -0500

    add admin table schema

--- sql/init.sql
+++ sql/init.sql
@@ -42,3 +42,11 @@ CREATE TABLE IF NOT EXISTS follows (
     FOREIGN KEY (followed) REFERENCES personas(persona_id),
     UNIQUE (follower, followed)
 );
+
+CREATE TABLE IF NOT EXISTS admin (
+    id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
