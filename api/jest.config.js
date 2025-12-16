6f8e2df0a21078ca00471afae1d6c07ab89e7a2d
argel6767 <argel6767@gmail.com>
2025-12-16 02:03:22 -0500

    update build pipline workflow

--- api/jest.config.js
+++ api/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   collectCoverageFrom: ['src/**/*.ts'],
+  reporters: ['default', 'jest-junit'],
+  coverageReporters: ['lcov', 'text'],
+  testResultsProcessor: 'jest-junit',
 };
