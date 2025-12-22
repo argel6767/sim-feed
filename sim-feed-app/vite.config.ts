dfd2954e4206e27ba0896021869542a651259773
argel6767 <argel6767@gmail.com>
2025-12-22 04:32:52 -0500

    update config

--- sim-feed-app/vite.config.ts
+++ sim-feed-app/vite.config.ts
@@ -5,4 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  ssr: {
+      noExternal: ["@pusher/push-notifications-web"],
+    },
+    optimizeDeps: {
+      include: ["@pusher/push-notifications-web"],
+    },
 });
