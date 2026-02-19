1eb6ccc27646464eda0626bf0df39f00f489e858
argel6767 <argel6767@gmail.com>
2026-02-18 21:20:01 -0500

    add SSMClient logic to fetch secrets in prod

---
+++ user-service/src/main/java/app/sim_feed/user_service/aws/models/ClerkProperties.java
@@ -0,0 +1,5 @@
+package app.sim_feed.user_service.aws.models;
+
+public record ClerkProperties(String clerkSecretKey) {
+    
+}
--- user-service/src/main/java/app/sim_feed/user_service/security/AuthenticateRequestOptionsConfiguration.java
+++ user-service/src/main/java/app/sim_feed/user_service/security/AuthenticateRequestOptionsConfiguration.java
@@ -2,10 +2,12 @@ package app.sim_feed.user_service.security;
 
 import com.clerk.backend_api.helpers.security.models.AuthenticateRequestOptions;
 
+import app.sim_feed.user_service.aws.models.ClerkProperties;
 import lombok.Setter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
@@ -18,8 +20,15 @@ public class AuthenticateRequestOptionsConfiguration {
     @Value("${sim.feed.domain}")
     private String simFeedDomain;
 
+    @Profile("dev")
     @Bean
-    public AuthenticateRequestOptions authenticateRequestOptions() {
+    public AuthenticateRequestOptions devAuthenticateRequestOptions() {
         return AuthenticateRequestOptions.secretKey(clerkSecretKey).authorizedParty(simFeedDomain).build();
+    }
+    
+    @Profile("prod")
+    @Bean
+    public AuthenticateRequestOptions prodAuthenticateRequestOptions(ClerkProperties clerkProperties) {
+        return AuthenticateRequestOptions.secretKey(clerkProperties.clerkSecretKey()).authorizedParty(simFeedDomain).build();
     }
 }
