e374da1731c3da07542f686d925bc92128e4a5ef
argel6767 <argel6767@gmail.com>
2026-02-18 21:45:10 -0500

    add build and test step for user service

---
+++ user-service/src/main/java/app/sim_feed/user_service/RootController.java
@@ -0,0 +1,15 @@
+package app.sim_feed.user_service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+@RequestMapping()
+public class RootController {
+
+    @RequestMapping("/")
+    public Map<String, String> index() {
+        return Map.of("message", "Welcome to the Sim-Feed's User Service!", "status", "OK");
+    }
+}
