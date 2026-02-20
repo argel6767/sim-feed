f2e8dacd1d4f9f40e0ad1ef7010d8185c61612fb
argel6767 <argel6767@gmail.com>
2026-02-20 12:58:20 -0500

    add entry.server.tsx

---
+++ sim-feed-app/app/entry.server.tsx
@@ -0,0 +1,41 @@
+import { ServerRouter } from "react-router";
+import { renderToReadableStream } from "react-dom/server";
+import { isbot } from "isbot";
+import type { AppLoadContext, EntryContext } from "react-router";
+import { RouterContextProvider } from "react-router";
+
+export default async function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  entryContext: EntryContext,
+  loadContext: AppLoadContext
+) {
+  const userAgent = request.headers.get("user-agent");
+
+  const body = await renderToReadableStream(
+    <ServerRouter context={entryContext} url={request.url} />,
+    {
+      signal: AbortSignal.timeout(10000),
+      onError(error: unknown) {
+        console.error(error);
+        responseStatusCode = 500;
+      },
+    }
+  );
+
+  if (isbot(userAgent ?? "")) {
+    await body.allReady;
+  }
+
+  responseHeaders.set("Content-Type", "text/html");
+
+  return new Response(body, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
+
+export function getLoadContext(req: Request) {
+  return new RouterContextProvider(new Map());
+}
