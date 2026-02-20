784cbc6ed1cbe83fefddf99fcbddb0bfd72da8b9
argel6767 <argel6767@gmail.com>
2026-02-20 12:33:48 -0500

    use vercel preset

--- sim-feed-app/react-router.config.ts
+++ sim-feed-app/react-router.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from "@react-router/dev/config";
+import {vercelPreset} from "@vercel/react-router/vite"
 
 export default {
   ssr: true,
   future: {
     v8_middleware: true,
   },
-  serverBuildFile: "server.ts",
+  presets: [vercelPreset],
 } satisfies Config;
