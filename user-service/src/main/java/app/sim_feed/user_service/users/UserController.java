259d087de6245321df95976a7ec7c4b3160ac005
argel6767 <argel6767@gmail.com>
2026-02-19 22:27:05 -0500

    also add rate limiter to user controller

--- user-service/src/main/java/app/sim_feed/user_service/users/UserController.java
+++ user-service/src/main/java/app/sim_feed/user_service/users/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.sim_feed.user_service.users.models.UserDto;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -17,6 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @PutMapping("/{id}")
+    @RateLimiter(name = "api-limiter")
     public UserDto updateUser(@PathVariable String id, @AuthenticationPrincipal String authenticatedUser, @RequestBody UserDto userDto) {
         return userService.updateUser(id, authenticatedUser, userDto);
     }
