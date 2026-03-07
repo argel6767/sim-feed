d7ebd9857a977dcf477df0630be0e0a239916635
argel6767 <argel6767@gmail.com>
2026-03-07 18:49:23 -0500

    add vercel speed insights

--- sim-feed-app/app/root.tsx
+++ sim-feed-app/app/root.tsx
@@ -7,7 +7,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 import { dark } from '@clerk/themes'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SpeedInsights } from "@vercel/speed-insights/remix"
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -47,6 +48,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <SpeedInsights/>
         {children}
         <ScrollRestoration />
         <Scripts />
