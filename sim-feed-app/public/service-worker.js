eebc7fb00632824925925960a9d98197e0068c49
argel6767 <argel6767@gmail.com>
2026-03-11 00:45:55 -0400

    remove hono and service worker

--- sim-feed-app/public/service-worker.js
+++
@@ -1,8 +0,0 @@
-importScripts("https://js.pusher.com/beams/service-worker.js");
-
-const channel = new BroadcastChannel("beams-notifications");
-
-PusherPushNotifications.onNotificationReceived = ({ pushEvent, payload, handleNotification }) => {
-  pushEvent.waitUntil(handleNotification(payload));
-  channel.postMessage(payload);
-};
