690642b4bb9955018f232ba5fc99f690039b6175
argel6767 <argel6767@gmail.com>
2026-03-11 00:33:31 -0400

    maybe no longer need hono workound?

--- sim-feed-app/app/server.ts
+++
@@ -1,7 +0,0 @@
-import { type AppLoadContext, RouterContextProvider } from "react-router";
-
-export function getLoadContext(
-  request: Request
-): RouterContextProvider {
-  return new RouterContextProvider(new Map<any, any>());
-}
